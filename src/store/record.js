5894c1de9ed6cf400d308676d76c6a28f0414c40
MaksymBohdan <maxymbohdan@gmail.com>
2020-10-25 21:54:04 +0200

    add directives

--- src/store/record.js
+++ src/store/record.js
@@ -1,3 +1,4 @@
+/* eslint-disable operator-linebreak */
 import firebase from 'firebase/app';
 
 export default {
@@ -9,6 +10,30 @@ export default {
           .database()
           .ref(`/users/${uid}/records`)
           .push(record);
+      } catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
+
+    async fetchRecords({ dispatch, commit }) {
+      try {
+        const uid = await dispatch('getUid');
+
+        const records =
+          (
+            await firebase
+              .database()
+              .ref(`/users/${uid}/records`)
+              .once('value')
+          ).val() || {};
+
+        const modifiedRecords = Object.keys(records).map((key) => ({
+          ...records[key],
+          id: key,
+        }));
+
+        return modifiedRecords;
       } catch (e) {
         commit('setError', e);
         throw e;
